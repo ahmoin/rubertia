96ab5b7b7cf63b5cbcc7385990450216a3a24697
Ahsan Moin <ahsanmoin05@gmail.com>
2024-03-15 04:47:10 -0400

    chore: switch to typescript

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber'
+
+const sizes = {
+  width: '100vw',
+  height: '100vh',
+};
+
+const Box: React.FC = () => {
+  return (
+    <mesh>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshBasicMaterial color={0xff0000} />
+    </mesh>
+  );
+};
+
+function App() {
+  return (
+    <Canvas
+      style={{ width: sizes.width, height: sizes.height }}
+      gl={{ alpha: false }}
+      camera={{
+        position: [0, 0, 3],
+        fov: 75,
+      }}
+    >
+      <Box />
+    </Canvas>
+  );
+}
+
+export default App
